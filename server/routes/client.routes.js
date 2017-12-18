04d3147e18ba6dae46ce66c24e152845d83f23b6
Ben Wiesner <benwiesner@gmail.com>
2017-12-17 21:43:19 -0600

    Fully Working

--- server/routes/client.routes.js
+++ server/routes/client.routes.js
@@ -41,15 +41,16 @@ router.post('/', function(req, res) {
 
 //POST and Update Client
 router.post('/:client_id', function(req, res) {
-	client.findById(req.params.client_id, function(err, client){
-		if(err) res.send(err);
-		client.firstName = req.body.firstName;
-		client.lastName = req.body.lastname;
-		client.telephone = req.body.telephone;
-		client.save(function (err, updatedClient){
+
+	client.findByIdAndUpdate(req.params.client_id, 
+		{
+		firstName : req.body.firstName,
+		lastName : req.body.lastName,
+		telephone : req.body.telephone
+		},
+		function(err, client){
 			if(err) res.send(err);
-			res.send(updatedClient);
-		});
+			res.send(client);
 	});
 });
 
